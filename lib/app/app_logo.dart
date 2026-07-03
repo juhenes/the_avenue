0caa3e8dbe67788b0997863bc9c7df38265f2e3d
DeogenesMaranan <maranandeogenes@gmail.com>
2026-07-03 10:16:38 +0800

    Simplify AppLogo widget by replacing custom decoration with image asset

--- lib/app/app_logo.dart
+++ lib/app/app_logo.dart
@@ -7,53 +7,14 @@ class AppLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    final logoColor = colorScheme.primary;
-    final backgroundColor = colorScheme.primaryContainer;
-
     return SizedBox(
       width: size,
       height: size,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(size * 0.28),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              width: size * 0.34,
-              height: size * 0.78,
-              decoration: BoxDecoration(
-                color: logoColor,
-                borderRadius: BorderRadius.circular(size * 0.18),
-              ),
-            ),
-            Positioned(
-              top: size * 0.16,
-              child: Container(
-                width: size * 0.18,
-                height: size * 0.18,
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                  shape: BoxShape.circle,
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: size * 0.16,
-              child: Container(
-                width: size * 0.12,
-                height: size * 0.12,
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                  shape: BoxShape.circle,
-                ),
-              ),
-            ),
-          ],
-        ),
+      child: Image.asset(
+        'assets/images/logo.png',
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
       ),
     );
   }
