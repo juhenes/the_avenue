4e574fc457f185ccbba17c8afc0d5a9ef28869be
DeogenesMaranan <maranandeogenes@gmail.com>
2026-07-03 11:15:59 +0800

    Change logo to theme chainging one

--- lib/app/app_logo.dart
+++ lib/app/app_logo.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'logo_painter.dart';
 
 class AppLogo extends StatelessWidget {
-  const AppLogo({super.key, this.size = 72});
+  const AppLogo({
+    super.key,
+    this.size = 72,
+  });
 
   final double size;
 
@@ -10,11 +14,17 @@ class AppLogo extends StatelessWidget {
     return SizedBox(
       width: size,
       height: size,
-      child: Image.asset(
-        'assets/images/logo.png',
-        width: size,
-        height: size,
+      child: FittedBox(
         fit: BoxFit.contain,
+        child: SizedBox(
+          width: 100,
+          height: 126.0736,
+          child: CustomPaint(
+            painter: LogoPainter(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/app/logo_painter.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class LogoPainter extends CustomPainter {
+  const LogoPainter({
+    required this.color,
+  });
+
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final path0 = Path();
+
+    path0.moveTo(size.width*0.1046650,size.height*0.7889981);
+    path0.cubicTo(size.width*0.1046650,size.height*0.7889981,size.width*0.02419600,size.height*0.8424504,0,size.height*0.8588568);
+    path0.cubicTo(size.width*0.01288610,size.height*0.8705255,size.width*0.02521915,size.height*0.8816678,size.width*0.03802229,size.height*0.8932269);
+    path0.cubicTo(size.width*0.05956364,size.height*0.8785094,size.width*0.08044134,size.height*0.8642306,size.width*0.1033653,size.height*0.8485480);
+    path0.lineTo(size.width*0.1033653,size.height*0.9999781);
+    path0.lineTo(size.width,size.height*0.9999781);
+    path0.lineTo(size.width,0);
+    path0.lineTo(size.width*0.1032271,0);
+    path0.cubicTo(size.width*0.1032271,size.height*0.004671872,size.width*0.1046650,size.height*0.7889981,size.width*0.1046650,size.height*0.7889981);
+    path0.close();
+    path0.moveTo(size.width*0.3935238,size.height*0.6270398);
+    path0.cubicTo(size.width*0.4112767,size.height*0.6132436,size.width*0.4273152,size.height*0.5980655,size.width*0.4447916,size.height*0.5840498);
+    path0.cubicTo(size.width*0.4488289,size.height*0.5808036,size.width*0.4553826,size.height*0.5780400,size.width*0.4609960,size.height*0.5777549);
+    path0.cubicTo(size.width*0.5188729,size.height*0.5748377,size.width*0.7519011,size.height*0.5651430,size.width*0.7613859,size.height*0.5651430);
+    path0.cubicTo(size.width*0.7592290,size.height*0.6694157,size.width*0.7706219,size.height*0.7727672,size.width*0.7877112,size.height*0.8769521);
+    path0.cubicTo(size.width*0.8057407,size.height*0.8750000,size.width*0.8233830,size.height*0.8730698,size.width*0.8420762,size.height*0.8710519);
+    path0.cubicTo(size.width*0.8303791,size.height*0.7682488,size.width*0.8167187,size.height*0.6661476,size.width*0.8179908,size.height*0.5620723);
+    path0.cubicTo(size.width*0.8471089,size.height*0.5610195,size.width*0.8758953,size.height*0.5599886,size.width*0.9044327,size.height*0.5589577);
+    path0.lineTo(size.width*0.9044327,size.height*0.5137963);
+    path0.cubicTo(size.width*0.8740702,size.height*0.5158142,size.width*0.8456433,size.height*0.5177224,size.width*0.8153914,size.height*0.5197403);
+    path0.cubicTo(size.width*0.8151425,size.height*0.4473811,size.width*0.8195670,size.height*0.3753948,size.width*0.8276139,size.height*0.3035182);
+    path0.cubicTo(size.width*0.8356884,size.height*0.2315757,size.width*0.8444819,size.height*0.1596991,size.width*0.8532478,size.height*0.08538779);
+    path0.cubicTo(size.width*0.8493764,size.height*0.08973065,size.width*0.8468047,size.height*0.09260397,size.width*0.8442607,size.height*0.09549921);
+    path0.cubicTo(size.width*0.7789177,size.height*0.1707098,size.width*0.7146254,size.height*0.2464906,size.width*0.6479827,size.height*0.3209554);
+    path0.cubicTo(size.width*0.5838288,size.height*0.3926566,size.width*0.5120703,size.height*0.4595982,size.width*0.4377402,size.height*0.5248070);
+    path0.cubicTo(size.width*0.4303570,size.height*0.5312774,size.width*0.4227802,size.height*0.5352474,size.width*0.4111108,size.height*0.5356642);
+    path0.cubicTo(size.width*0.3663413,size.height*0.5372214,size.width*0.2001217,size.height*0.5450079,size.width*0.1617676,size.height*0.5466529);
+    path0.cubicTo(size.width*0.1569560,size.height*0.5468503,size.width*0.1521168,size.height*0.5466749,size.width*0.1464757,size.height*0.5466749);
+    path0.lineTo(size.width*0.1464757,size.height*0.03421653);
+    path0.lineTo(size.width*0.9575533,size.height*0.03421653);
+    path0.lineTo(size.width*0.9575533,size.height*0.9677794);
+    path0.lineTo(size.width*0.1460056,size.height*0.9677794);
+    path0.cubicTo(size.width*0.1460056,size.height*0.9649500,size.width*0.1463927,size.height*0.8179505,size.width*0.1463927,size.height*0.8179505);
+    path0.cubicTo(size.width*0.1463927,size.height*0.8179505,size.width*0.3146863,size.height*0.6883444,size.width*0.3935514,size.height*0.6270398);
+    path0.close();
+    path0.moveTo(size.width*0.1465310,size.height*0.7577645);
+    path0.lineTo(size.width*0.1465310,size.height*0.5925382);
+    path0.cubicTo(size.width*0.2204187,size.height*0.5891604,size.width*0.2942234,size.height*0.5858045,size.width*0.3711252,size.height*0.5822951);
+    path0.cubicTo(size.width*0.2987031,size.height*0.6439507,size.width*0.2247878,size.height*0.7011318,size.width*0.1465310,size.height*0.7577645);
+    path0.close();
+    path0.moveTo(size.width*0.5057103,size.height*0.5330102);
+    path0.cubicTo(size.width*0.6032132,size.height*0.4452536,size.width*0.6911761,size.height*0.3546017,size.width*0.7726682,size.height*0.2601553);
+    path0.cubicTo(size.width*0.7679673,size.height*0.3470126,size.width*0.7632663,size.height*0.4334313,size.width*0.7585377,size.height*0.5210563);
+    path0.cubicTo(size.width*0.6764096,size.height*0.5249386,size.width*0.5927606,size.height*0.5289086,size.width*0.5057103,size.height*0.5330102);
+    path0.close();
+
+    final paint0 = Paint()
+      ..style = PaintingStyle.fill
+      ..isAntiAlias = true
+      ..color = color;
+
+    canvas.drawPath(path0, paint0);
+  }
+
+  @override
+  bool shouldRepaint(covariant LogoPainter oldDelegate) {
+    return oldDelegate.color != color;
+  }
+}
